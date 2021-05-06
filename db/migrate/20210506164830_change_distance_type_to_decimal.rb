562823a1989f1f87043b1be313ffd953e94b7491
Peter <peter.karpinski@gdsoft.info>
2021-05-06 15:10:30 -0400

    Change migration name

--- db/migrate/20210506164830_change_distance_type_to_decimal.rb
+++ db/migrate/20210506164830_change_distance_type_to_decimal.rb
@@ -1,4 +1,4 @@
-class ChangeDistanceTypeToFloat < ActiveRecord::Migration[6.1]
+class ChangeDistanceTypeToDecimal < ActiveRecord::Migration[6.1]
   def change
     change_column :activities, :distance, :decimal, precision: 10, scale: 2
   end
